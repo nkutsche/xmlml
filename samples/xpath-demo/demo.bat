181515acf0341b8a0bf738ba4327664ca37c6d49
Nico Kutscherauer <kutscherauer@data2type.de>
2024-08-07 18:23:40 +0200

    Fixes XPath demo

--- samples/xpath-demo/demo.bat
+++ samples/xpath-demo/demo.bat
@@ -5,7 +5,7 @@ set xpath=%~1
 if not "%xpath%"=="" set xpath=xpath='%xpath%'
 
 
-mvn -q -f ../../pom.xml exec:exec -Dexec.executable="java" -Dexec.args="%J_OPTS% -cp %%classpath net.sf.saxon.Transform -init:com.nkutsche.xslt.pkg.handler.PackageManager -xsl:%~dp0\demo.xsl -it -s:%~dp0\xpaths.xml %xpath%"
+mvn -q -P demo -f ../../pom.xml exec:exec -Dexec.executable="java" -Dexec.args="%J_OPTS% -cp %%classpath net.sf.saxon.Transform -init:com.nkutsche.xslt.pkg.handler.PackageManager -xsl:%~dp0\demo.xsl -it -s:%~dp0\xpaths.xml %xpath%"
 
 :: XPath:
 :: mvn exec:java  -Dexec.mainClass=net.sf.saxon.Transform -Dexec.args="-init:com.nkutsche.xslt.pkg.handler.PackageManager -xsl:src/test/xspec/samples/sample-test-xpath.xsl -s:src/test/xspec/samples/dummy.xml -o:target/debug-out.xml"
