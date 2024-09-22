8e4356a18d40bb14c8d3da479f29c3e0e9e61ef9
Nico Kutscherauer <kutscherauer@data2type.de>
2024-09-22 18:48:44 +0200

    Minor updates

--- run-xspec.bat
+++ run-xspec.bat
@@ -4,4 +4,4 @@ if not "%1"=="" set XSPEC=%1
 
 if "%1"=="#all" set XSPEC=
 
-mvn com.nkutsche:xspec-maven-plugin:run-xspec@run-xspec -Drun.xspecs=%XSPEC%
+mvn compile com.nkutsche:xspec-maven-plugin:run-xspec@run-xspec -Drun.xspecs=%XSPEC%
